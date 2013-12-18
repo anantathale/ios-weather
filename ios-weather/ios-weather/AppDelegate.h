61107263b0ea76e25cfe3b211ac6e83d4d73ba1e
Anant <anant.athale@gmail.com>
2013-12-17 21:55:23 -0700

    Initial Commit
    Initial Commit

---
+++ ios-weather/ios-weather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-weather
+//
+//  Created by Anant Athale on 12/17/13.
+//  Copyright (c) 2013 Anant Athale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-weather/ios-weather/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ios-weather
+//
+//  Created by Anant Athale on 12/17/13.
+//  Copyright (c) 2013 Anant Athale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
